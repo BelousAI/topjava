a31ea1e27fe031254e6dd624c572864496bcf76c
Anton <belous.nsk@gmail.com>
2020-03-02 20:51:40 +0300

    Implement UserRepository

---
+++ src/main/java/ru/javawebinar/topjava/util/UsersUtil.java
@@ -0,0 +1,27 @@
+package ru.javawebinar.topjava.util;
+
+import ru.javawebinar.topjava.model.Role;
+import ru.javawebinar.topjava.model.User;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UsersUtil {
+
+    public static final List<User> USERS = Arrays.asList(
+            new User("Anton1", "anton1@gmail.com", "123", Role.ROLE_USER),
+            new User("Anton2", "anton2@gmail.com", "456", Role.ROLE_USER),
+            new User("Anton3", "anton3@gmail.com", "788", Role.ROLE_USER),
+            new User("Anton4", "anton4@gmail.com", "561", Role.ROLE_USER),
+            new User("Anton5", "anton5@gmail.com", "654", Role.ROLE_USER)
+    );
+
+    public static List<User> getUsers(Collection<User> users) {
+        return users.stream()
+                .sorted(Comparator.comparing(User::getName))
+                .collect(Collectors.toList());
+    }
+}
