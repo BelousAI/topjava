3cadb79c9486d4f11887f1ba4653e44a2aba8fd9
Anton <belous.nsk@gmail.com>
2020-03-19 23:05:28 +0300

    HW03 Test MealService part-I

---
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -0,0 +1,42 @@
+package ru.javawebinar.topjava;
+
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.model.User;
+
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+
+public class MealTestData {
+    public static final int USER_ID = START_SEQ;
+    public static final int ADMIN_ID = START_SEQ + 1;
+
+    public static final int USER_MEAL_ID = START_SEQ + 2;
+    public static final int ADMIN_MEAL_ID = START_SEQ + 3;
+
+    public static final Meal USER_MEAL = new Meal(USER_MEAL_ID,
+            LocalDateTime.of(2019, 1, 30, 10, 0),"Завтрак", 500);
+
+    public static final Meal ADMIN_MEAL = new Meal(ADMIN_MEAL_ID,
+            LocalDateTime.of(2019, 1, 30, 13, 0),"Обед", 1000);
+
+    public static Meal getNew() {
+        return new Meal(null, LocalDateTime.now(), "mealDescription", 1000);
+    }
+
+    public static void assertMatch(Meal actual, Meal expected) {
+        assertThat(actual).isEqualToIgnoringGivenFields(expected, "dateTime");
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Meal... expected) {
+        assertMatch(actual, Arrays.asList(expected));
+    }
+
+    public static void assertMatch(Iterable<Meal> actual, Iterable<Meal> expected) {
+        assertThat(actual).usingElementComparatorIgnoringFields("dateTime").isEqualTo(expected);
+    }
+
+
+}
---
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -0,0 +1,76 @@
+package ru.javawebinar.topjava.service;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.bridge.SLF4JBridgeHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit4.SpringRunner;
+import ru.javawebinar.topjava.model.Meal;
+
+import static org.junit.Assert.*;
+import static ru.javawebinar.topjava.MealTestData.*;
+
+@ContextConfiguration({
+        "classpath:spring/spring-app.xml",
+        "classpath:spring/spring-db.xml"
+})
+@RunWith(SpringRunner.class)
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+public class MealServiceTest {
+
+    static {
+        // Only for postgres driver logging
+        // It uses java.util.logging and logged via jul-to-slf4j bridge
+        SLF4JBridgeHandler.install();
+    }
+
+    @Autowired
+    private MealService service;
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void create() {
+        Meal newMeal = getNew();
+        Meal created = service.create(newMeal, USER_ID);
+        Integer newId = created.getId();
+        newMeal.setId(newId);
+        assertMatch(created, newMeal);
+        assertMatch(service.get(newId, USER_ID), newMeal);
+    }
+
+    @Test
+    public void get() {
+        Meal meal = service.get(USER_MEAL_ID, USER_ID);
+        assertMatch(meal, USER_MEAL);
+    }
+
+    @Test
+    public void delete() {
+
+    }
+
+    @Test
+    public void getBetweenHalfOpen() {
+    }
+
+    @Test
+    public void getAll() {
+    }
+
+    @Test
+    public void update() {
+    }
+}
