18ec10f13fcfdbbd9c99bce0602e8068f0e8fc8e
antonbelous <belous.nsk@gmail.com>
2020-04-19 21:40:33 +0300

    HW04-3 part I

--- src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jpa/JpaMealRepository.java
@@ -1,5 +1,6 @@
 package ru.javawebinar.topjava.repository.jpa;
 
+import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.javawebinar.topjava.model.Meal;
@@ -43,10 +44,11 @@ public class JpaMealRepository implements MealRepository {
 
     @Override
     public Meal get(int id, int userId) {
-        return (Meal) em.createNamedQuery(Meal.BY_ID)
+        List<Meal> meals = em.createNamedQuery(Meal.BY_ID, Meal.class)
                 .setParameter("id", id)
                 .setParameter("userId", userId)
-                .getSingleResult();
+                .getResultList();
+        return DataAccessUtils.singleResult(meals);
     }
 
     @Override
